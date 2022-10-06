53c1a37a6aaaf69411002a591a37e576d457e1df
Ramanjaneyulu <ramanjaneyulu.kdk@gmail.com>
2022-10-06 07:12:15 +0530

    Adding some more examples in exceptions

---
+++ app/src/standards/Example1.java
@@ -0,0 +1,18 @@
+package standards;
+
+public class Example1 {
+
+	public static int age = 30;
+	
+	public static String firstName = "abc";
+	
+	public static void main(String[] args) {
+		
+		Example1 example1 = new Example1();
+	}
+	
+	public void calculateSimpleInterest()
+	{
+		
+	}
+}
